75abe7ea8173c7e154d902f4015275d3ef6c9ef0
fguzman <fguzman@IBSCOMP124>
2024-09-12 17:02:46 -0400

    ApplicationContext and ConnectionString Implementation

---
+++ IMDB.Infraestructure/Context/ApplicationContext.cs
@@ -0,0 +1,45 @@
+﻿using IMDB.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMDB.Infraestructure.Context
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Actor> Actor { get; set; }
+        public DbSet<Cast> Cast { get; set; }
+        public DbSet<Director> Director { get; set; }
+        public DbSet<Gender> Gender { get; set; }
+        public DbSet<MediaActor> MediaActor { get; set; }
+        public DbSet<MediaContent> MediaContent { get; set; }
+        public DbSet<MediaDirector> MediaDirector { get; set; }
+        public DbSet<MediaGender> MediaGender { get; set; }
+        public DbSet<Producer> Producer { get; set; }
+        public DbSet<Rating> Rating { get; set; }
+        public DbSet<Rol> Rol {  get; set; }
+        public DbSet<Domain.Entities.Type> Type {  get; set; }
+
+        public ApplicationContext() { }
+
+        public ApplicationContext (DbContextOptions<ApplicationContext> options ) : base (options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=MSI; Database=IMDB; TrustServerCertificate=true", m => m.MigrationsAssembly(typeof(ApplicationContext).Assembly.FullName));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+           //Algo falta aqui
+        }
+
+    }
+}
---
+++ IMDB.Infraestructure/Core/BaseRepository.cs
@@ -0,0 +1,37 @@
+﻿using IMDB.Domain.Core;
+using IMDB.Infraestructure.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMDB.Infraestructure.Core
+{
+    public class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : class
+    {
+        private readonly ApplicationContext _context;
+        private DbSet<TEntity> _entities;
+
+        public BaseRepository(ApplicationContext context)
+        {
+            _context = context;
+        }
+        Task<bool> IBaseRepository<TEntity>.DeleteAsync(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<bool> IBaseRepository<TEntity>.ExitsAsync(Expression<Func<TEntity, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<bool> IBaseRepository<TEntity>.SaveAsync(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
