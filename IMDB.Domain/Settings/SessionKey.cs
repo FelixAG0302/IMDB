d3c2a98e3d9d30f268a5dee324787fa7ae1b5d9c
fguzman <fguzman@IBSCOMP124>
2024-09-12 15:28:45 -0400

    Intraestructure library creation

---
+++ IMDB.Domain/Settings/SessionKey.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMDB.Domain.Settings
+{
+    public class SessionKey
+    {
+        public string UserKey { get; set; }
+    }
+}
